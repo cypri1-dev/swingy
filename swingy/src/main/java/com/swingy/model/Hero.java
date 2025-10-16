82e658b0d59524f2b5647f3db6792ed7bb1c522f
Cyprien Ferreira <cyferrei@f2r4s11.clusters.42paris.fr>
2025-10-16 17:54:58 +0200

    [MODIF]: minor modit testing map size with different hero level

--- swingy/src/main/java/com/swingy/model/Hero.java
+++ swingy/src/main/java/com/swingy/model/Hero.java
@@ -8,7 +8,7 @@ public class Hero extends Characters {
 
 	public Hero(String type, String name, String characterClass) {
 		super(type, name, characterClass);
-		this.setLevel(0);
+		this.setLevel(1);
 		this.setXp(0);
 		this.movement = new GameMovement();
 	}
