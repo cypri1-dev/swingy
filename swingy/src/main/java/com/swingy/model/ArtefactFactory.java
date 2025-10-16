ea5f9c91aa745fa65f81e95ab7ff96b2859c4a06
Cyprien Ferreira <cyferrei@f1br6s3.clusters.42paris.fr>
2025-10-16 14:03:23 +0200

    [ADD]: add ArtefactFactory - switch statement inside will return the right artefact

---
+++ swingy/src/main/java/com/swingy/model/ArtefactFactory.java
@@ -0,0 +1,33 @@
+package com.swingy.model;
+
+import static com.swingy.utils.Constants.*;
+import com.swingy.model.Armor;
+import com.swingy.model.Helm;
+import com.swingy.model.Weapon;
+
+
+public class ArtefactFactory {
+	private static ArtefactFactory instance;
+
+	private ArtefactFactory() {}
+
+	public static ArtefactFactory getInstance() {
+		if (instance == null)
+			instance = new ArtefactFactory();
+		return instance;
+	}
+
+	public Artefact newArtefact(String type, String name, int bonus) {
+		switch(type) {
+			case ARMOR_TYPE:
+				return new Armor(type, name, bonus);
+			case HELM_TYPE:
+				return new Helm(type, name, bonus);
+			case WEAPON_TYPE:
+				return new Weapon(type, name, bonus);
+			default:
+				System.out.println(RED_BOLD + "Error: unknown type!" + RESET);
+		}
+		return null;
+	}
+}
