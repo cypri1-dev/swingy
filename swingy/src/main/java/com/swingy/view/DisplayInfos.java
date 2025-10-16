08eb87dd21bedef3aa3da81344f7802a3643bf7d
Cyprien Ferreira <cyferrei@f1br6s3.clusters.42paris.fr>
2025-10-16 14:06:15 +0200

    [ADD]: add new method printArtefact - little modif to display bag content from hero

--- swingy/src/main/java/com/swingy/view/DisplayInfos.java
+++ swingy/src/main/java/com/swingy/view/DisplayInfos.java
@@ -1,5 +1,6 @@
 package com.swingy.view;
 
+import com.swingy.model.Artefact;
 import com.swingy.model.Characters;
 import static com.swingy.utils.Constants.*;
 
@@ -14,7 +15,12 @@ public class DisplayInfos {
 		System.out.println(ITALIC + "[attack]: " + RESET + hero.getAttack());
 		System.out.println(ITALIC + "[defense]: " + RESET + hero.getDefense());
 		System.out.println(ITALIC + "[hitPoint]: " + RESET + hero.getHitPoint());
-		System.out.println(ITALIC + "[bag]: " + RESET + hero.getArtefacts());
+		System.out.println("------- BAG CONTENT -------");
+		// System.out.println(ITALIC + "[bag]: " + RESET + hero.getArtefacts());
+		for (Artefact item : hero.getArtefacts()) {
+			System.out.println(item.getName());
+		}
+		System.out.println("------- ----------- -------");
 		System.out.println(ITALIC + "[x]: " + RESET + hero.getCoordinates().getX());
 		System.out.println(ITALIC + "[y]: " + RESET + hero.getCoordinates().getY());
 		System.out.println(ITALIC + "[prevX]: " + RESET + hero.getCoordinates().getPrevX());
@@ -32,11 +38,20 @@ public class DisplayInfos {
 		System.out.println(ITALIC + "[attack]: " + RESET + hero.getAttack());
 		System.out.println(ITALIC + "[defense]: " + RESET + hero.getDefense());
 		System.out.println(ITALIC + "[hitPoint]: " + RESET + hero.getHitPoint());
-		System.out.println(ITALIC + "[bag]: " + RESET + hero.getArtefacts());
+		// System.out.println(ITALIC + "[bag]: " + RESET + hero.getArtefacts());
 		// System.out.println(ITALIC + "[x]: " + RESET + hero.getCoordinates().getX());
 		// System.out.println(ITALIC + "[y]: " + RESET + hero.getCoordinates().getY());
 		// System.out.println(ITALIC + "[prevX]: " + RESET + hero.getCoordinates().getPrevX());
 		// System.out.println(ITALIC + "[prevY]: " + RESET + hero.getCoordinates().getPrevY());
 		System.out.println(DEBUG_BOLD + "--- END ENEMY DATA ---" + RESET);
+	}
+
+	public static void printArtefact(Artefact item) {
+		System.out.println(DEBUG_BOLD + "--- ARTEFACT DATA ---" + RESET);
+		System.out.println(ITALIC + "[type]: " + RESET + item.getType());
+		System.out.println(ITALIC + "[name]: " + RESET + item.getName());
+		System.out.println(ITALIC + "[bonus]: " + RESET + item.getBonus());
+		System.out.println(ITALIC + "[isEquipped]: " + RESET + item.getIsEquipped());
+		System.out.println(DEBUG_BOLD + "--- END ARTEFACT DATA ---" + RESET);
 	}
 }
