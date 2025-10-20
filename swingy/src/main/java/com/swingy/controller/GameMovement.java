9c6d453968f207309043f3d195417688d40843db
Cyprien Ferreira <cyferrei@f1br10s1.clusters.42paris.fr>
2025-10-21 01:06:30 +0200

    [ADD]: add checkFight method and checkEndLevel method - also add logic of movement on map - detecting enemies and printing them on map - for now, i let all the monsters visible - will be removed later

--- swingy/src/main/java/com/swingy/controller/GameMovement.java
+++ swingy/src/main/java/com/swingy/controller/GameMovement.java
@@ -2,24 +2,75 @@ package com.swingy.controller;
 
 import com.swingy.model.Characters;
 import com.swingy.model.Map;
-import static com.swingy.utils.Constants.*;;
+import static com.swingy.utils.Constants.*;
 
 public class GameMovement {
+
+	private void checkFight(Characters hero, Map map) {
+
+		if (!map.map[hero.getCoordinates().getPrevX()][hero.getCoordinates().getPrevY()].contains("!")) {
+			map.map[hero.getCoordinates().getPrevX()][hero.getCoordinates().getPrevY()] = BLUE + "*" + RESET;
+		}
+
+		for (Characters enemy : map.getListEnemies()) {
+			if (hero.getCoordinates().getX() == enemy.getCoordinates().getX() && hero.getCoordinates().getY() == enemy.getCoordinates().getY()) {
+
+				System.out.println(RED_BOLD + "FIGHT!" + RESET);
+				System.out.println(enemy.getCharacterClass() + " | " + enemy.getCoordinates().getX() + ", " + enemy.getCoordinates().getY());
+
+				map.map[hero.getCoordinates().getX()][hero.getCoordinates().getY()] = BLUE + "!" + RESET;
+				return;
+			}
+		}
+		map.map[hero.getCoordinates().getX()][hero.getCoordinates().getY()] = SYMBOL_MAIN_HERO;
+	}
+
 	private void checkEndLevel(Characters hero, Map map) {
-		if (hero.getCoordinates().getX() == 0 || hero.getCoordinates().getPrevX() == map.getSize() - 1 || hero.getCoordinates().getPrevY() == 0 || hero.getCoordinates().getY() == map.getSize() - 1)
+		if (hero.getCoordinates().getX() == 0 || hero.getCoordinates().getX() == map.getSize() - 1 || hero.getCoordinates().getY() == 0 || hero.getCoordinates().getY() == map.getSize() - 1)
 			System.out.println(DEBUG_BOLD + "LVL COMPLETED!" + RESET);
 	}
 
+
 	public void moveNorth(Characters hero, Map map) {
+
 		hero.getCoordinates().setPrevX(hero.getCoordinates().getX());
 		hero.getCoordinates().setPrevY(hero.getCoordinates().getY());
-		map.map[hero.getCoordinates().getX()][hero.getCoordinates().getY()] = "*";
+
 		hero.getCoordinates().setX(-1);
-		map.map[hero.getCoordinates().getX()][hero.getCoordinates().getY()] = SYMBOL_MAIN_HERO;
+
+		checkFight(hero, map);
 		checkEndLevel(hero, map);
 	}
-	public void moveEast(Characters hero) {}
-	public void moveSouth(Characters hero) {}
-	public void moveWest(Characters hero) {}
+
+
+	public void moveEast(Characters hero, Map map) {
+		hero.getCoordinates().setPrevX(hero.getCoordinates().getX());
+		hero.getCoordinates().setPrevY(hero.getCoordinates().getY());
+
+		hero.getCoordinates().setY(+1);
+		
+		checkFight(hero, map);
+		checkEndLevel(hero, map);
+	}
+
+	public void moveSouth(Characters hero, Map map) {
+		hero.getCoordinates().setPrevX(hero.getCoordinates().getX());
+		hero.getCoordinates().setPrevY(hero.getCoordinates().getY());
+		
+		hero.getCoordinates().setX(+1);
+		
+		checkFight(hero, map);
+		checkEndLevel(hero, map);
+	}
+
+	public void moveWest(Characters hero, Map map) {
+		hero.getCoordinates().setPrevX(hero.getCoordinates().getX());
+		hero.getCoordinates().setPrevY(hero.getCoordinates().getY());
+		
+		hero.getCoordinates().setY(-1);
+		
+		checkFight(hero, map);
+		checkEndLevel(hero, map);
+	}
 
 }
