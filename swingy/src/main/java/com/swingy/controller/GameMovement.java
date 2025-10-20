d95c8725fea5ceb59c5113ebed1bfd0c90f614f3
Cyprien Ferreira <cyferrei@f1br8s20.clusters.42paris.fr>
2025-10-20 15:25:37 +0200

    [ADD]: add methopd to check end of level - also add moveNorth function

--- swingy/src/main/java/com/swingy/controller/GameMovement.java
+++ swingy/src/main/java/com/swingy/controller/GameMovement.java
@@ -1,11 +1,25 @@
 package com.swingy.controller;
 
-import com.swingy.model.Hero;
+import com.swingy.model.Characters;
+import com.swingy.model.Map;
+import static com.swingy.utils.Constants.*;;
 
 public class GameMovement {
-	public void moveNorth(Hero hero) {}
-	public void moveEast(Hero hero) {}
-	public void moveSouth(Hero hero) {}
-	public void moveWest(Hero hero) {}
+	private void checkEndLevel(Characters hero, Map map) {
+		if (hero.getCoordinates().getX() == 0 || hero.getCoordinates().getPrevX() == map.getSize() - 1 || hero.getCoordinates().getPrevY() == 0 || hero.getCoordinates().getY() == map.getSize() - 1)
+			System.out.println(DEBUG_BOLD + "LVL COMPLETED!" + RESET);
+	}
+
+	public void moveNorth(Characters hero, Map map) {
+		hero.getCoordinates().setPrevX(hero.getCoordinates().getX());
+		hero.getCoordinates().setPrevY(hero.getCoordinates().getY());
+		map.map[hero.getCoordinates().getX()][hero.getCoordinates().getY()] = "*";
+		hero.getCoordinates().setX(-1);
+		map.map[hero.getCoordinates().getX()][hero.getCoordinates().getY()] = SYMBOL_MAIN_HERO;
+		checkEndLevel(hero, map);
+	}
+	public void moveEast(Characters hero) {}
+	public void moveSouth(Characters hero) {}
+	public void moveWest(Characters hero) {}
 
 }
