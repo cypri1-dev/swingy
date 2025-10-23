24d1dd666fea544c90bdedb9869b0440d23789cf
Cyprien Ferreira <cyferrei@f2r7s23.clusters.42paris.fr>
2025-10-23 19:16:30 +0200

    [ADD]: add new logic fight class

---
+++ swingy/src/main/java/com/swingy/controller/FightLogic.java
@@ -0,0 +1,13 @@
+package com.swingy.controller;
+
+import com.swingy.model.Characters;
+import static com.swingy.utils.Constants.*;
+
+public class FightLogic {
+
+	public static void fight(Characters enemy, Characters hero) {
+		do {
+			
+		} while (enemy.getHitPoint() > 0 || hero.getHitPoint() > 0);
+	}
+}
