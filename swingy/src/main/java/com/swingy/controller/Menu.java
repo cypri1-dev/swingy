fd211326bf777c806182471b0e7971cd7a809c84
Cyprien Ferreira <cyferrei@f4r10s2.clusters.42paris.fr>
2025-10-23 13:30:40 +0200

    [MODIF/CLEAN]: cleaned menu section

--- swingy/src/main/java/com/swingy/controller/Menu.java
+++ swingy/src/main/java/com/swingy/controller/Menu.java
@@ -12,7 +12,6 @@ import com.swingy.model.MapFactory;
 import com.swingy.view.DisplayController;
 import com.swingy.view.DisplayInfos;
 import com.swingy.model.Characters;
-import com.swingy.model.Map;
 
 public class Menu {
 
@@ -23,6 +22,95 @@ public class Menu {
 	Menu(Game game) {
 		this.ref = game;
 		this.herosName = new ArrayList<String>();
+	}
+
+	private void creationOption() {
+		DisplayController.getInstance().clearTerminal();
+		createHero();
+	}
+
+	private void heroesManagementOption() {
+		DisplayController.getInstance().clearTerminal();
+		DisplayController.getInstance().printSlow(VIEW_HEROS);
+		viewMyHeros();
+		if (this.ref.getListAvaible().isEmpty()) {
+			DisplayController.getInstance().printSlow(ENTER_BACK);
+			scanner.nextLine();
+		}
+		else
+			removeHero();
+	}
+
+	private void arenaOption() {
+		DisplayController.getInstance().clearTerminal();
+		if (this.ref.getListAvaible().isEmpty()) {
+			DisplayController.getInstance().printSlow(WARNING);
+			scanner.nextLine();
+		}
+		else {
+			DisplayController.getInstance().printMyHeros(ref);
+			String selectedHero = "";
+			do {
+				DisplayController.getInstance().printSlow(SELECT_HERO);
+				selectedHero = scanner.nextLine();
+				if (selectedHero.equals("x") || selectedHero.equals("X"))
+					return;
+			} while (!this.herosName.contains(selectedHero));
+
+			for (Characters hero : this.ref.getListAvaible()) {
+				if (selectedHero.equals(hero.getName())) {
+					hero.getCoordinates().setStart((hero.getLevel() - 1) * 5 + 10 - (hero.getLevel() % 2));
+					this.ref.setSelectedHero(hero);
+					this.ref.setActualMap(MapFactory.getInstance().newMap(hero));
+					break;
+				}
+			}
+			DisplayController.getInstance().clearTerminal();
+			DisplayController.getInstance().displayMap(this.ref);
+			DisplayInfos.printHero(this.ref.getMainHero());
+			scanner.nextLine();
+		}
+	}
+
+	private void exitOption() {
+		DisplayController.getInstance().clearTerminal();
+		DisplayController.getInstance().printSlow(OUT_MSG);
+		scanner.close();
+		System.exit(0);
+	}
+
+	private void d_VOption() {
+		DisplayController.getInstance().clearTerminal();
+		String optionSelected = "";
+		do {
+			DisplayController.getInstance().printSlow(A_SIMPLE);
+			optionSelected = scanner.nextLine();
+		} while (!optionSelected.equals("1") && !optionSelected.equals("2"));
+
+		switch (optionSelected) {
+			case "1":
+				boolean validInput = false;
+				int s___d = 0;
+				D_V_M___E = true;
+
+				do {
+					try {
+						DisplayController.getInstance().printSlow(SPEED);
+						s___d = Integer.parseInt(scanner.nextLine());
+						validInput = true;
+					}
+					catch (NumberFormatException e) {
+						System.out.println("NOP");
+					}
+				} while (!validInput);
+				D_V_S___D_T_T = s___d;
+
+			case "2":
+				break;
+			
+			default:
+				break;
+		}
 	}
 
 	public void launchGame() {
@@ -41,87 +129,20 @@ public class Menu {
 
 		switch (option) {
 			case "1":
-				DisplayController.getInstance().clearTerminal();
-				createHero();
+				creationOption();
 				break;
 			case "2":
-				DisplayController.getInstance().clearTerminal();
-				DisplayController.getInstance().printSlow(VIEW_HEROS);
-				viewMyHeros();
-				if (this.ref.getListAvaible().isEmpty()) {
-					DisplayController.getInstance().printSlow(ENTER_BACK);
-					scanner.nextLine();
-				}
-				else
-					removeHero();
+				heroesManagementOption();
 				break;
 			case "3":
-				DisplayController.getInstance().clearTerminal();
-				if (this.ref.getListAvaible().isEmpty()) {
-					DisplayController.getInstance().printSlow(WARNING);
-					scanner.nextLine();
-				}
-				else {
-					DisplayController.getInstance().printMyHeros(ref);
-					String selectedHero = "";
-					do {
-						DisplayController.getInstance().printSlow(SELECT_HERO);
-						selectedHero = scanner.nextLine();
-						if (selectedHero.equals("x") || selectedHero.equals("X"))
-							return;
-					} while (!this.herosName.contains(selectedHero));
-
-					for (Characters hero : this.ref.getListAvaible()) {
-						if (selectedHero.equals(hero.getName())) {
-							hero.getCoordinates().setStart((hero.getLevel() - 1) * 5 + 10 - (hero.getLevel() % 2));
-							this.ref.setSelectedHero(hero);
-							this.ref.setActualMap(MapFactory.getInstance().newMap(hero));
-							break;
-						}
-					}
-					DisplayController.getInstance().clearTerminal();
-					DisplayController.getInstance().displayMap(this.ref);
-					DisplayInfos.printHero(this.ref.getMainHero());
-					scanner.nextLine();
-				}
+				arenaOption();
 				break;
 			case "4":
-				DisplayController.getInstance().clearTerminal();
-				DisplayController.getInstance().printSlow(OUT_MSG);
-				scanner.close();
-				System.exit(0);
+				exitOption();
 			case "S":
-				DisplayController.getInstance().clearTerminal();
-				String optionSelected = "";
-				do {
-					DisplayController.getInstance().printSlow(A_SIMPLE);
-					optionSelected = scanner.nextLine();
-				} while (!optionSelected.equals("1") && !optionSelected.equals("2"));
-
-			switch (optionSelected) {
-				case "1":
-					boolean validInput = false;
-					int s___d = 0;
-					D_V_M___E = true;
-
-					do {
-						try {
-							DisplayController.getInstance().printSlow(SPEED);
-							s___d = Integer.parseInt(scanner.nextLine());
-							validInput = true;
-						}
-						catch (NumberFormatException e) {
-							System.out.println("NOP");
-						}
-					} while (!validInput);
-					D_V_S___D_T_T = s___d;
-
-				case "2":
-					break;
-
-				default:
-					break;
-			}
+				d_VOption();
+			default:
+				break;
 		}
 	}
 
