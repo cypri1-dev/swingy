8e897167a785b129a0d81afc49c13ebe44b2befe
Cyprien Ferreira <cyferrei@f1r5s18.clusters.42paris.fr>
2025-11-10 17:13:17 +0100

    [TESTS]: testing some function from swing

--- swingy/src/main/java/com/swingy/App.java
+++ swingy/src/main/java/com/swingy/App.java
@@ -1,60 +1,17 @@
 package com.swingy;
 
-
-import javax.swing.*;          // Pour JFrame, JLabel, JButton, JTextField
-
-import static com.swingy.utils.Constants.*;
-
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
+import static com.swingy.utils.Constants.*;
 import com.swingy.controller.Game;
 import com.swingy.controller.Import;
 import com.swingy.view.DisplayController;
+import com.swingy.view.ExempleBoxLayout;
 
 public class App {
 	public static void main(String[] args) {
-		// windows creation
-		// JFrame frame = new JFrame("Conversion Celcius -> Fahrenheit");
-		// frame.setSize(400, 150);
-		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// frame.setLayout(null);
-
-		// // label asking for temperature
-		// JLabel label = new JLabel("Enter temperature in °C :");
-		// label.setBounds(20, 20, 200, 25);
-		// frame.add(label);
-
-		// // field to enter temperature
-		// JTextField textField = new JTextField();
-		// textField.setBounds(220, 20, 100, 25);
-		// frame.add(textField);
-
-		// // button to make conversion
-		// JButton button = new JButton("Convert");
-		// button.setBounds(150, 60, 100, 25);
-		// frame.add(button);
-
-		// // label diplaying result
-		// JLabel reslutLabel = new JLabel("");
-		// reslutLabel.setBounds(20, 100, 350, 25);
-		// frame.add(reslutLabel);
-
-		// // button action
-		// button.addActionListener(new ActionListener() {
-		// 	@Override
-		// 	public void actionPerformed(ActionEvent e) {
-		// 		try {
-		// 			double celcius = Double.parseDouble(textField.getText());
-		// 			double fahreinheit = celcius * 9 / 5 + 32;
-		// 			reslutLabel.setText(celcius + "°C = " + fahreinheit + "°F");
-		// 		} catch (NumberFormatException ex) {
-		// 			reslutLabel.setText("Please enter valid value !");
-		// 		}
-		// 	}
-		// });
-
-		// frame.setVisible(true);
-
 		if (args.length != 1) {
 			DisplayController.getInstance().printNormal("Please specify the mode: console - gui");
 			return;
@@ -63,17 +20,19 @@ public class App {
 		switch (args[0]) {
 			case CONSOLE:
 				Game rpg = new Game();
-				boolean loadingSave = false;
-		
-				loadingSave = Import.fileChecker();
+				boolean loadingSave = Import.fileChecker();
 				if (loadingSave)
 					Import.fileParser(rpg);
-				while(true) {
+				while (true) {
 					rpg.getMenu().launchGame();
 				}
-				
+
 			case GUI:
-				DisplayController.getInstance().printNormal("GUI comming soon...");
+				DisplayController.getInstance().printNormal("GUI coming soon...");
+				JFrame window = new ExempleBoxLayout();
+				window.setSize(500, 400);
+				window.setLocationRelativeTo(null);
+				window.setVisible(true);
 				break;
 
 			default:
